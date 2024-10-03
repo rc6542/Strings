e74ebe5974a8eeda12c5c0fa39682d93bd4b84ba
Ryan Chen <rc6542@princeton.edu>
2024-10-03 16:24:24 -0400

    replace

--- replace.c
+++ replace.c
@@ -21,7 +21,7 @@ static size_t replaceAndWrite(const char *pcLine,
                               const char *pcFrom, const char *pcTo)
 {
    size_t replacements;
-   const char *occurence;
+   char *occurence;
 
    if(*pcFrom == '\0') {
       printf("%s", pcLine);
