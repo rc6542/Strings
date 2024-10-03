79e786d3570083ac658170d73df0de8e27e350a0
Ryan Chen <rc6542@princeton.edu>
2024-10-02 20:22:51 -0400

    str.a

--- stra.c
+++ stra.c
@@ -34,17 +34,61 @@ char* Str_copy(char pcDest[], const char pcSrc[]) {
 /* Concatenates the contents of source to the end of destination and 
 returns it. */
 char* Str_concat(char pcDest[], const char pcSrc[]) {
+    size_t i = 0, j = 0;
+    assert(pcSrc != NULL && pcDest != NULL);
 
+    while (pcDest[i] != '\0') {
+        i++;
+    }
+
+    while (pcSrc[j] != '\0') {
+        pcDest[i+j] = pcSrc[j];
+        j++;
+    }
+    pcDest[i+j] = '\0';  
+    return pcDest;
 }
 
-/* Compares str1 and str1, returns 0 if they are equal. */
+/* Compares str1 and str2, returns 0 if they are equal.  */
 char* Str_compare(char str1[], const char str2[]) {
+    size_t i = 0;
+    assert(str1 != NULL && str2 != NULL);
 
+    while (str1[i] != '\0' && str2[i] != '\0') {
+        if (str1[i] != str2[i]) {
+            return (int)(str1[i] - str2[i]);
+        }
+        i++;
+    }
+
+    return (int)(str1[i] - str2[i]);
 }
 
 /* Returns a pointer to the first occurrence in pcNeedle in the entire 
 sequence of characters pcHaystack, or a null pointer if the pcNeedle is
 not found in pcHaystack.*/
-char *Str_search(const char *pcHaystack, const char *pcNeedle) {
+char *Str_search(const char pcHaystack[], const char pcNeedle[]) {
+    size_t i, j;
+    assert(pcHaystack != NULL && pcNeedle != NULL);
 
+    /* if pcNeedle is empty, return pcHaystack */
+    if (pcNeedle[0] == '\0') {
+        return (char *)pcHaystack;
+    }
+
+    /* iterate over each character in pcHaystack */
+    for (i = 0; pcHaystack[i] != '\0'; i++) {
+        /* check if pcNeedle matches starting at pcHaystack[i] */
+        j = 0;
+        while (pcNeedle[j] != '\0' && pcHaystack[i + j] == pcNeedle[j]) {
+            j++;
+        }
+
+        /* if we reached the end of pcNeedle, we found a full match */
+        if (pcNeedle[j] == '\0') {
+            return (char *)&pcHaystack[i];
+        }
+    }
+
+    return NULL;
 }
