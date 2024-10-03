e55e3a9469d805c78bc79b6de8cd025b7cb40813
Ryan Chen <rc6542@princeton.edu>
2024-10-02 20:55:48 -0400

    str.h

--- str.h
+++ str.h
@@ -19,11 +19,11 @@ returns it. */
 char* Str_concat(char pcDest[], const char pcSrc[]);
 
 /* Compares str1 and str2, returns 0 if they are equal. */
-int Str_compare(const char *str1, const char *str2);
+int Str_compare(const char str1[], const char str2[]);
 
 /* Returns a pointer to the first occurrence in pcNeedle in the entire 
 sequence of characters pcHaystack, or a null pointer if the pcNeedle is
-not found in pcHaystack.*/
+not found in pcHaystack. */
 char *Str_search(const char *pcHaystack, const char *pcNeedle);
 
 #endif
